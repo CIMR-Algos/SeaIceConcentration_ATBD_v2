9b23ceb4019ad3d2bf9604b184623becdab808a8
Thomas Lavergne <thomas.lavergne@met.no>
2024-06-10 22:58:17 +0200

    Upate the prototype implementation notebooks (Offline, SIC algorithm, Performance Assessment)

--- algorithm/run_CIMR_L2_Sea_Ice_Concentration_algorithm.py
+++ algorithm/run_CIMR_L2_Sea_Ice_Concentration_algorithm.py
@@ -31,16 +31,17 @@ if __name__ == '__main__':
                                      description='run CIMR L2 SIC algorithm prepared in CIMR DEVALGO')
     parser.add_argument('L1B_PATH', help='Path to the CIMR L1B file.')
     parser.add_argument('-o', help='Directory where to write the L2 SIC file', default=default_l2_dir)
+    parser.add_argument('--no-oza-adjust', help='Do NOT do OZA adjustement.', dest='use_oza_adjust', action='store_false')
+    parser.add_argument('--aux-dir', help='Where to read the.', dest='use_oza_adjust', action='store_false')
     parser.add_argument('-g', help='Target grid of the L2 SIC algorithm.', default=default_l2_grid)
     args = parser.parse_args()
 
     # run the L2 SIC algorithm via the notebook
-    try:
-        run_notebook(args.L1B_PATH, args.o, args.g)
-    except pm.exceptions.PapermillExecutionError as pme:
-        print(pme)
-        sys.exit("Failed with Papermill Execution Error")
-    except Exception as ex:
-        print(ex)
-        sys.exit("Failed with general exception.")
+    print('++++++++ START L2 Sea Ice Concentration ++++')
+    sic_notebook = './CIMR_L2_Sea_Ice_Concentration_algorithm.ipynb'
+    sic_notebook_out = os.path.join(args.t, os.path.basename(sic_notebook).replace('.ipynb','_out.ipynb'))
+    sic_notebook_par = {'l1b_path': oza_l1b_file, 'l2_dir': out_dir, 'l2_grid': args.g, 'use_oza_adjust': args.use_oza_adjust,
+                        'aux_dir': tmp_dir, 'tuning_method':'CIMRL1B-PERFEED', 'l1b_archive_dir': os.path.dirname(oza_l1b_file)}
+    _ = pm.execute_notebook(sic_notebook,sic_notebook_out,parameters=sic_notebook_par,cwd=os.path.dirname(sic_notebook))
+    print('++++++++ END L2 Sea Ice Concentration ++++')
 
